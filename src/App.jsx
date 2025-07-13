695dbadc9eb0f250e41fc085d975a255aab068b0
JananiThura <jananithurai.official@gmail.com>
2025-07-13 19:57:54 +0530

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes';
+import AppRoutes from './routes/adminroutes.jsx';
 import './assets/css/admin-theme.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
--- src/routes/adminroutes.jsx
+++ src/routes/adminroutes.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
-import AdminDashboard from './dashboard/admin/Admindashboard';
-import AdminUsers from './dashboard/admin/AdminUsers';
-import NotFound from './pages/NotFound';
-import Login from './components/Login'; // ✅ Make sure path is correct
-import Employee from './dashboard/Employee';
+import Home from '../pages/Home';
+import AdminLogin from '../pages/AdminLogin';
+import AdminDashboard from '../dashboard/admin/Admindashboard';
+import AdminUsers from '../dashboard/admin/AdminUsers';
+import NotFound from '../pages/NotFound';
+import Login from '../components/Login'; // ✅ Make sure path is correct
+import Employee from '../dashboard/Employee';
 const AppRoutes = () => {
   return (
     <Routes>

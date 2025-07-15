0d6479c0e6f81d3cec74a0ddb5b4ad3ce86288e4
S-Abinaya123 <888abinayas@gmail.com>
2025-07-15 19:24:24 +0530

    updated employee dashboard

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './dashboard/admin/AdminDashboard';
-import AdminUsers from './dashboard/admin/AdminUsers';
-
-// Import your components/pages
-import Home from './pages/Home';
-
 import NotFound from './pages/NotFound';
-import Login from './components/Login'; // ✅ Make sure path is correct
+import Login from './components/Login';
 import Employee from './dashboard/Employee';
+
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Public Routes */}
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<AdminLogin />} />
+      <Route path="/login" element={<Login />} />
 
       {/* Admin Routes */}
       <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/admin/users" element={<AdminUsers />} />
+
+      {/* Employee Dashboard */}
+      <Route path="/employee" element={<Employee />} />
 
       {/* Catch-All */}
-      <Route path="/login" element={<Login />} /> {/* ✅ Login Route */}
       <Route path="*" element={<NotFound />} />
-      <Route path="/Employee" element={<Employee />} />
     </Routes>
   );
 };
